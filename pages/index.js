8cb846d5b07ee93f114c893c2acb3bf9e6820008
Stephen <105071093+stephenokyere233@users.noreply.github.com>
2023-01-17 20:33:21 +0000

    [add] more refactoring

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import React, { useContext } from "react";
 import Hero from "../components/hero/Hero";
-import Load from "../components/loader/Load";
 import { AppContext } from "../context/context";
 import { useGetSongsByGenreQuery } from "../store/services/shazamCore";
 
@@ -9,7 +8,6 @@ export default function Home() {
   const { genreQuery } = useContext(AppContext);
 
   const { data, isFetching, error } = useGetSongsByGenreQuery(genreQuery);
-
   return (
     <>
       <Hero discover={data} isFetching={isFetching} error={error} />
